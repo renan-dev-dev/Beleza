6d9210b484871d7f10a80406b7e2718673281916
renan-dev-dev <renantx12@gmail.com>
2025-07-25 13:48:26 -0300

    add: favicon fot.png

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/sala1/',
+  base: '/sala1/', // Nome do seu repositório
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
   }
 })
