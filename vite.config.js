3cb7da11f2e2cb2cb3f4ee5cdfad579d442d85b6
renan-dev-dev <renantx12@gmail.com>
2025-07-25 15:02:21 -0300

    novos componentes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/sala1/', // Nome do seu repositório
+  base: '/sala1/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
