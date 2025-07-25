ea84cbe6cd72c78dad71bcf0913961f02d44ff60
renan-dev-dev <renantx12@gmail.com>
2025-07-25 19:04:06 -0300

    deploy: ajustes finais

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Beleza/', // nome exato do seu repositório no GitHub
+  base: '/Beleza/', // deve ser exatamente assim!
 })
